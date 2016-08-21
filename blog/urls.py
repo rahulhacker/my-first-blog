f20681953550a5ea311725c6330e16c65f3e0640
Rahul Kumar <rahulkumar.10.06.1991@gmail.com>
2016-08-21 21:23:20 +0530

    Website 1st changes

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^$',views.post_data,name='post_data'),
+    url(r'^list$',views.list, name='list'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+#this is my first page
 def post_list(request):
     return render(request, 'blog/index_2.html', {})
 
+# this function will return the list of data which is there in django list
+def list(request):
+    return render(request,'blog/list1.html', {})
 
+# this function will be there for to fill a form to the model
